eb1d37501b2ad58983c0e90e95dc90bd2954b7e3
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-09-10 16:38:13 -0700

    Added tests for Channel and List method

---
+++ lib/channel.rb
@@ -0,0 +1,20 @@
+class Channel < Recipient
+  attr_reader :slack_id, :name, :topic, :member_count
+  @@channels_list = []
+
+  def initialize(slack_id, name, topic, member_count)
+    super(slack_id, name)
+    @topic = topic
+    @member_count = member_count
+    @@channels_list << self
+  end
+
+  def self.list
+    return @@channels_list
+  end
+
+
+
+
+end
+
--- test/user_test.rb
+++ test/user_test.rb
@@ -6,34 +6,34 @@ describe "user class" do
             slack_id = 4
             name = "Dom"
             real_name = "Dominic Tareto"
-
+            
             slacker = User.new(slack_id, name, real_name)
             expect(slacker.slack_id).must_equal slack_id
             expect(slacker.name).must_equal name
             expect(slacker.real_name).must_equal real_name  
         end
     end
-
+    
     describe "self.list method" do
         it "should return an array of users" do
             slack_id = 4
             name = "Dom"
             real_name = "Dominic Tareto"
             slacker = User.new(slack_id, name, real_name)
-
+            
             expect(User.list).must_be_kind_of Array
             #expect(list).must_be_kind_of Array
         end
-
+        
         it "should return instances of User" do
             #expect(@user).must_be_kind_of User
         end
     end
-
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
 end
