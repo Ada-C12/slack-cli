0f610dd6e2d6b92f2eb30b886ea1e62c5b619884
stupendousC <CarolineWuKaplan@gmail.com>
2019-09-10 16:54:06 -0700

    testing making new workspae & @all_channels, :-|

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -3,9 +3,9 @@ require_relative 'user.rb'
 require_relative 'recipient.rb'
 
 class Workspace
-
+  
   attr_reader :all_users, :all_channels
-
+  
   def initialize()
     # make all User instances
     @all_users = []
@@ -16,9 +16,18 @@ class Workspace
       real_name = value[:real_name]
       @all_users << User.new(id: id, name: name, real_name: real_name)
     end
-
+    
     # make all Channel instances
-
+    @all_channels = []
+    giant_hash = Channel.channels_in_giant_hash
+    giant_hash.each do |key, value|
+      id = key
+      puts "using Id = #{id}"
+      name = value[:name]
+      topic = value[:topic]
+      member_count = value[:member_count]
+      @all_channels << Channel.new(id:id, name:name, topic: topic, member_count: member_count)
+    end
   end
-
+  
 end
