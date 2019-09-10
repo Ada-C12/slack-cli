81a7177105c0f8bfabb81b2a7ced9e3a565fd660
Julia Bouvier <juliabouv@gmail.com>
2019-09-10 11:52:08 -0700

    Configured VCR to filter_sensitive_data for slack token

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,19 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require 'vcr'
+require "dotenv"
+Dotenv.load
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
   config.cassette_library_dir = "test/cassettes"
   config.hook_into :webmock
+  
+  # Don't leave our token lying around in a cassette file.
+  config.filter_sensitive_data("<SLACK_API_TOKEN>") do
+    ENV["SLACK_API_TOKEN"]
+  end
 end
+
+
