231750327aa131863282f05a2e0698ed22f6434f
Mira <calopter@gmail.com>
2019-09-10 13:36:05 -0700

    verifies authorization

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,9 +1,24 @@
 #!/usr/bin/env ruby
 
+require 'dotenv'
+require 'httparty'
+
+Dotenv.load
+
+KEY = ENV["SLACK_API_KEY"]
+URL = "https://slack.com/api/conversations.list"
+
 def main
   puts "Welcome to the Ada Slack CLI!"
 
   # TODO project
+
+  query_params = {
+    token: KEY,
+  }
+
+  response = HTTParty.get URL, query: query_params
+  puts response
 
   puts "Thank you for using the Ada Slack CLI"
 end
