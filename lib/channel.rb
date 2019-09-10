975cf00ab888a94d67ae1a021bf48b3872c3d7f2
Julia Bouvier <juliabouv@gmail.com>
2019-09-10 16:14:24 -0700

    Initialized Channel class. Added list method

---
+++ lib/channel.rb
@@ -0,0 +1,24 @@
+
+
+class Channel < Recipient
+  attr_reader :topic, :member_count
+  
+  def initialize(topic: , member_count: , slack_id: , name: )
+    super(slack_id: slack_id, name: name)
+    @topic = topic
+    @member_count = member_count
+  end
+  
+  # def details
+  # end
+  
+  def self.list
+    response = self.get("https://slack.com/api/channels.list", ENV["SLACK_TOKEN"])
+    
+    # ap response
+    members = response["channels"].map do |channel|
+      self.new(slack_id: channel["id"], name: channel["name"], topic: channel["topic"]["value"], member_count: channel["members"].length )
+    end
+    return members
+  end
+end
