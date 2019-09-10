07fa5f11a1e21623ce80a10b16e63291fb047758
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-09-10 12:31:08 -0700

    Copied ENV file to lib: Authenticated Token Works

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,11 +1,22 @@
 #!/usr/bin/env ruby
+require "httparty"
+require "dotenv"
+Dotenv.load
 
+BASE_URL = "https://slack.com/api/channels.list"
+KEY = ENV["SLACK_API_TOKEN"]
 def main
-  puts "Welcome to the Ada Slack CLI!"
+  query = {
+  token: KEY
+  }
+  response = HTTParty.get(BASE_URL, query: query)
+  #puts "Welcome to the Ada Slack CLI!"
 
   # TODO project
-
-  puts "Thank you for using the Ada Slack CLI"
+  return response
+  #puts "Thank you for using the Ada Slack CLI"
 end
+
+puts main
 
 main if __FILE__ == $PROGRAM_NAME
