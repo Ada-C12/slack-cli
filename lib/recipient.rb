d6b04bd34fe9254af8a2f934e1fa9ae0af1fd53e
ohcloud <cloudylopez@icloud.com>
2019-09-10 16:51:23 -0700

    ran into weird errors, pushing so dani can pull.

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -2,8 +2,8 @@ require 'httparty'
 require 'awesome_print'
 require 'dotenv'
 require 'table_print'
-require_relative "../lib/channel.rb"
-require_relative "../lib/user.rb"
+# require_relative "../lib/channel.rb"
+# require_relative "../lib/user.rb"
 require_relative "../lib/slack.rb"
 
 Dotenv.load
@@ -11,23 +11,10 @@ Dotenv.load
 module SlackCLI
   class Recipient
     attr_reader :slack_id, :name
+    
     def initialize(slack_id, name)
       @slack_id = slack_id
       @name = name
     end
   end
 end
-
-#CHANNELS_URL = https://slack.com/api/channels.list
-#MESSAGE_URL = https://api.slack.com/methods/chat.postMessage
-URL = 'https://slack.com/api/users.list'
-TOKEN = ENV['SLACK_TOKEN']
-
-
-def self.get
-  rec_response = HTTParty.get(URL, query: { token: TOKEN })
-  ap rec_response["members"]
-end
-
-ap self.get 
-
--- lib/slack.rb
+++ lib/slack.rb
@@ -5,17 +5,43 @@ require 'dotenv'
 require 'table_print'
 Dotenv.load
 
-# require_relative "../lib/recipient.rb"
+require_relative "../lib/recipient.rb"
 # require_relative "../lib/user.rb"
 # require_relative "../lib/channel.rb"
 
+module SlackCLI
+  
+  class Slack
+    @@URL = 'https://slack.com/api/users.list'
+    @@TOKEN = ENV['SLACK_TOKEN']
+    
+    attr_reader :users, :channels, :selected
+    
+    def initialize(users, channels, selected)
+      @users = users
+      @channels = channels
+      @selected = selected
+    end
+    
+    
+    def users_list(url, token)
+      users_response = HTTParty.get(@@URL, query: { token: @@TOKEN })
+      ap users_response["members"]
+    end
+    
+    
+  end # of class
+  
+end # of module
 
 def main
+  ap users_list(@@URL, @@TOKEN)
   puts "Welcome to the Ada Slack CLI!"
-  
-  # TODO project
-  
-  puts "Thank you for using the Ada Slack CLI"
+  puts "Thank you For using the Ada Slack CLI"
 end
+# main if __FILE__ == $PROGRAM_channel
+SlackCLI::Slack.main
 
-main if __FILE__ == $PROGRAM_NAME
+
+# CHANNELS_URL = https://slack.com/api/channels.list
+# MESSAGE_URL = https://api.slack.com/methods/chat.postMessage
