9565f899453402c11ad2da8627bd1d4ff6a032bc
Natalie Tapias <nataliemtapias@gmail.com>
2019-09-10 16:21:28 -0700

    We have troubleshooted VCR. We have written one test that makes one call to the slack channels api. Test is passing.

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -19,4 +19,36 @@ describe "Recipient" do
     end
   end  
   
+  describe "get" do
+    it "should be an instance of HTTParty" do
+      # response = SlackCLI::Recipient.new(slack_id: "1", name: "Katie")
+      # p response.get
+      # p SlackCLI::Recipient.get
+      VCR.use_cassette("recipient") do
+        expect(SlackCLI::Recipient.get).must_be_instance_of HTTParty::Response
+      end
+    end
+  end  
+  
+  
+  # describe "get" do
+  #   it "can find channels" do
+  #     VCR.use_cassette("list_channels") do
+  #       location = "Seattle"
+  #       response = get_location(location)
+  
+  #       expect(response["Seattle"]).wont_be_nil
+  #       expect(response["Seattle"][:lon]).must_equal "-122.3300624"
+  #       expect(response["Seattle"][:lat]).must_equal "47.6038321"
+  #     end
+  #   end   
+  
+  #   it "will raise an exception if the search fails" do
+  #     VCR.use_cassette("location_find") do
+  #       location = ""
+  #       expect {
+  #       response = get_location(location)
+  #     }.must_raise SearchError
+  #   end
+  # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,12 +7,27 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require 'webmock/minitest'
+require 'dotenv'
+
+Dotenv.load
+
+
 require 'vcr'
-# require_relative 'recipient_test'
+require_relative 'recipient_test'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
   config.cassette_library_dir = "test/cassettes"
   config.hook_into :webmock
+  
+  config.default_cassette_options = {
+  :record => :new_episodes,
+  :match_requests_on => [:method, :uri, :body],
+}
+config.filter_sensitive_data("<SLACK_TOKEN>") do
+  ENV["SLACK_TOKEN"]
 end
+end
+
