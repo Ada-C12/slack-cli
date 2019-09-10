c5e1f80a6bb2485f3fded7162af35bb010936b94
Georgina Sanchez Buenrostro <cpgeorgina.sanchez@gmail.com>
2019-09-10 14:18:26 -0700

    set-up

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,7 +1,23 @@
 #!/usr/bin/env ruby
+require 'dotenv'
+require 'httparty'
+
+# Tell dotenv to look for the .env file
+Dotenv.load
+
+URL =	"https://slack.com/api/channels.list"
+KEY = ENV['SLACK_TOKEN']
 
 def main
   puts "Welcome to the Ada Slack CLI!"
+
+  response = HTTParty.get(URL, query: {
+    token: KEY
+  })
+
+  response["channels"].each do |channel|
+    puts channel["name"]
+  end
 
   # TODO project
 
