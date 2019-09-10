0ff76b2abc7349eee1e155a0096f10d49178131d
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-09-10 12:32:28 -0700

    successfully pulled conversation list

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,10 +1,23 @@
 #!/usr/bin/env ruby
+require 'dotenv'
+require 'httparty'
+require 'awesome_print'
+
+Dotenv.load
+
+URL = "https://slack.com/api/conversations.list"
+KEY = ENV['SLACK_API_TOKEN']
 
 def main
+  
+  query_param = {
+    token: KEY
+  }
   puts "Welcome to the Ada Slack CLI!"
-
+  
   # TODO project
-
+  response = HTTParty.get(URL, query: query_param)
+  puts response
   puts "Thank you for using the Ada Slack CLI"
 end
 
