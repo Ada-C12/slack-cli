e9c203a24fbaa1ae8c1708471fb3dd2f87a23b60
Monick <monick.keo@gmail.com>
2019-09-10 15:00:06 -0700

    was able to get env file working

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,39 +1,32 @@
-#!/usr/bin/env ruby
 require 'dotenv'
 require 'httparty'
 require 'awesome_print'
 Dotenv.load 
 
-class 
+ 
 def main
   puts "Welcome to the Ada Slack CLI! What would you like to do?"
   puts "list channels, list users, or quit"
 
   user_input = gets.chomp 
-
   case user_input 
   when "list channels"
     list_channels
   when "list users"
     list_users
-  when quit 
+  when "quit" 
+    puts "Thank you for using the Ada Slack CLI"
     exit 
   end 
-
-  puts "Thank you for using the Ada Slack CLI"
 end
-main
+# main
 
 def list_channels
   method_url = "https://slack.com/api/channels.list"
   query_params = {
-    # key: ENV["SLACK-TOKEN"]
-    token: "xoxp-741657849778-756645927222-757736307830-f34cbca9bdbe14b7faed9993862f15a6"
+    token: ENV["SLACK_TOKEN"]
   }
   response = HTTParty.get(method_url, query: query_params)
-
-  #DO NOT REMOVE - getting one name for one channel 
-  # ap response.parsed_response["channels"][0]["name"]
 
   channels = response.parsed_response["channels"]
   i = 0 
@@ -43,13 +36,12 @@ def list_channels
     puts channel_name
   end 
 end 
-# list_channels
+list_channels
 
 def list_users
   method_url = "https://slack.com/api/users.list"
   query_params = {
-    # key: ENV["SLACK-TOKEN"]
-    token: "xoxp-741657849778-756645927222-757736307830-f34cbca9bdbe14b7faed9993862f15a6"
+    token: ENV["SLACK_TOKEN"]
   }
   response = HTTParty.get(method_url, query: query_params)
 
