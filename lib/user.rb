cc78eca1fc98b3173916a6b348fa7d7dac1d63ee
geli-gel <angele.zam@gmail.com>
2019-09-10 15:42:36 -0700

    created self.list in User and test

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,9 @@
+require 'httparty'
+require 'awesome_print'
+require 'dotenv'
+
+Dotenv.load
+
 module SlackCLI
   class User < Recipient
     attr_reader :real_name
@@ -5,6 +11,31 @@ module SlackCLI
     def initialize(slack_id:, name:, real_name:)
       super(slack_id: slack_id, name: name)
       @real_name = real_name
+    end
+
+    def self.list
+      # Call the API to get users
+      url = "https://slack.com/api/users.list"
+      key = ENV["API_TOKEN"]
+
+      query = {
+        token: key
+      }
+
+      response = HTTParty.get(url, query: query)
+
+      users = response["members"].map do |member|
+        # pull ID, Name, and Real Name
+        slack_id = member["id"]
+        name = member["name"]
+        real_name = member["real_name"]
+          
+        # for each user, inititialize User with those things
+        SlackCLI::User.new(slack_id: slack_id, name: name, real_name: real_name)
+      end
+
+      return users
+
     end
     
   end
--- test/user_test.rb
+++ test/user_test.rb
@@ -5,7 +5,7 @@ describe "User" do
     it "can be initialized as child class of Recipient" do
       slack_id = "TD83838H"
       name = "SlackBot"
-      real_name = "Mr. Slack Bot"
+      real_name = "SlackBot"
 
       new_recipient = SlackCLI::User.new(
         slack_id: slack_id, 
@@ -15,8 +15,17 @@ describe "User" do
 
       expect(new_recipient.class < SlackCLI::Recipient).must_equal true
     end
+  end
 
+  describe "self.list" do
+    it "creates a list of at least 1 user" do
+      VCR.use_cassette("list_users") do
+        users_list = SlackCLI::User.list
 
+        expect(users_list).must_be_instance_of Array
+        expect(users_list.first).must_be_instance_of SlackCLI::User
+      end
+    end
 
   end
 
