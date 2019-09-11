9ffa927a4ed48049f4aff25863087817025fb2e0
Kristina <kristina.tanya@gmail.com>
2019-09-11 08:57:24 -0700

    Formatted CLI Main to re-print commands when given bad input

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,35 +1,38 @@
+require_relative "workspace"
 require_relative "user"
-# require_relative ""
-# require_relative ""
+require_relative "channel"
+require_relative "acceptor"
 
 def main
   
   puts "Welcome to the Ada Slack CLI!"
-  # display info about how many channels & user there are 
-  # provide 3 options for the user
-  puts "Please select from the following options: 
+  # TO-DO: Display total count of users and channels
+
+  prompt = "Please select from the following options: 
   List Users
   List Channels
   Quit"
 
-  command = gets.chomp.downcase
-
-  # loop through if user inputs  aljdflkasdf
-  # if command == "quit"
-  #   exit
-  # elsif command == "list users"
-  # elseif 
-  case command
-  when "quit"
-    exit
-  when "list users"
-    Slack::User.print_list
-  when "list channels"
+  puts prompt
+  
+  while command = gets.chomp.downcase
+    case command
+    when "quit"
+      exit
+    # when "list users"
+      # TO-DO: call print_list method for user
+      # break - to avoid asking for more input
+    # when "list channels"
+      # TO-DO: call print_list method for channel
+      # break - to avoid asking for more input
+    else
+      puts "Error: Invalid selection. Returning to main menu...."
+      puts prompt
+    end
   end
-
- 
   puts "Thank you for using the Ada Slack CLI"
 
 end
 
 main if __FILE__ == $PROGRAM_NAME
+
