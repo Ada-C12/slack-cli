50a1cf5c1fc0a644926483ea84e6d4897864b683
stupendousC <CarolineWuKaplan@gmail.com>
2019-09-11 15:30:55 -0700

    can add User and Channel instances from API, tested w/ 100% coverage

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -7,27 +7,11 @@ class Workspace
   attr_reader :all_users, :all_channels
   
   def initialize()
-    # make all User instances
-    @all_users = []
-    giant_hash = User.users_in_giant_hash
-    giant_hash.each do |key, value|
-      id = key
-      name = value[:name]
-      real_name = value[:real_name]
-      @all_users << User.new(id: id, name: name, real_name: real_name)
-    end
-    
+    # load all User instances
+    @all_users = User.load_all
+
     # make all Channel instances
-    @all_channels = []
-    giant_hash = Channel.channels_in_giant_hash
-    giant_hash.each do |key, value|
-      id = key
-      puts "using Id = #{id}"
-      name = value[:name]
-      topic = value[:topic]
-      member_count = value[:member_count]
-      @all_channels << Channel.new(id:id, name:name, topic: topic, member_count: member_count)
-    end
+    @all_channels = Channel.load_all
   end
   
 end
