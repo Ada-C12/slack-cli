58d9be5880d920b091c85e975563267c823f417e
dora1405 <li.dora05@gmail.com>
2019-09-10 20:32:52 -0700

    Wave 1, channel.rb self.list method, updated variable names in the method

--- lib/channel.rb
+++ lib/channel.rb
@@ -2,6 +2,7 @@
 require 'httparty'
 require  'dotenv'
 require 'awesome_print'
+require 'table_print'
 
 Dotenv.load
 
@@ -13,9 +14,8 @@ class Channel
   KEY = ENV['SLACK_TOKEN']
   
   
-  def initialize
-    # (slack_id:, name:, topic:, member_count:)
-    # super(slack_id, name)
+  def initialize(slack_id:, name:, topic:, member_count:)
+    super(slack_id, name)
     @topic = topic
     @member_count = member_count
   end
@@ -29,33 +29,34 @@ class Channel
   def self.list
     response = HTTParty.get(URL, query: {token: KEY})
     
-    channel_name = []
-    channel_topic = []
-    channel_member_ct = []
-    channel_slack_id = []
+    @name = []
+    @topic = []
+    @member_count = []
+    @slack_id = []
+
     channels = response["channels"]
     
-    channel_name = channels.map do |channel|
+    name = channels.map do |channel|
       channel['name']
     end
     
-    channel_topic = channels.map do |channel|
+    topic = channels.map do |channel|
       channel['topic']['value']
     end
     
-    channel_member_ct = channels.map do |channel|
+    member_count = channels.map do |channel|
       channel['num_members']
     end
     
-    channel_slack_id = channels.map do |channel|
+    @slack_id = channels.map do |channel|
       channel['id']
     end
     
-    channel_name.length.times do |i|
-      puts "Name: #{channel_name[i]},\n
-      Topic: #{channel_topic[i]},\n
-      Member count: #{channel_member_ct[i]},\n
-      Slack ID: #{channel_slack_id[i]}"
+    name.length.times do |i|
+      puts "Name: #{name[i]},\n
+      Topic: #{topic[i]},\n
+      Member count: #{member_count[i]},\n
+      Slack ID: #{@slack_id[i]}"
     end
   end
 end
