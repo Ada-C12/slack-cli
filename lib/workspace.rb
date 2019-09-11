1dbdd9f55f3f96cf57855b985a62bafbf8eba4f6
Kristina <kristina.tanya@gmail.com>
2019-09-11 09:44:27 -0700

    Created scaffolding for Workspace

---
+++ lib/workspace.rb
@@ -0,0 +1,27 @@
+module Slack
+  class Workspace
+    # attr_reader
+    def initialize
+      @users = []
+      @channels = []
+      @selected = []
+    end
+
+    # def select_user
+      # code
+    # end
+
+    # def select_channel
+      # code
+    # end
+
+    # def show_details
+      # code
+    # end
+
+    # def send_message
+      # code
+    # end
+
+  end 
+end 
