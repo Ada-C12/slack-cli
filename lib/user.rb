dccd54960a5b117f77fde99d55bde81df7dce588
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-09-11 09:32:52 -0700

    Resolved more merge conflicts

--- lib/user.rb
+++ lib/user.rb
@@ -1,16 +1,8 @@
 require 'httparty'
 
 class User < Recipient
-<<<<<<< HEAD
-  URL = 'https://slack.com/api/users.list'
-  KEY = ENV['SLACK_API_TOKEN']
-
-    attr_reader :slack_id, :name, :real_name
-    @@users_list = []
-=======
   attr_reader :slack_id, :name, :real_name
   @@users_list = []
->>>>>>> fc314b644f484bcafa1efd1eb1531638e94b2c51
 
   def initialize(slack_id, name, real_name)
     super(slack_id, name) 
@@ -18,7 +10,6 @@ class User < Recipient
     @@users_list << self
   end
 
-<<<<<<< HEAD
     def self.list
       query = {
         token: KEY
@@ -26,10 +17,5 @@ class User < Recipient
       response = HTTParty.get(URL, query: query)
       return response
     end
-=======
-  def self.list
-    return @@users_list
-  end
->>>>>>> fc314b644f484bcafa1efd1eb1531638e94b2c51
 
 end
