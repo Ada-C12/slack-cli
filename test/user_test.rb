65a50fe31e29e297ea28b3e33426c92d46a55f25
Julia Bouvier <juliabouv@gmail.com>
2019-09-11 15:02:06 -0700

    Implemented #details tests

--- test/user_test.rb
+++ test/user_test.rb
@@ -1,10 +1,10 @@
 require_relative "test_helper"
 
 describe "User" do 
+  before do
+    @user = User.new(real_name: "Bob", slack_id: "1234", name: "Bobiscool")
+  end
   describe "initialize" do
-    before do
-      @user = User.new(real_name: "Bob", slack_id: "1234", name: "Bobiscool")
-    end
     it "creates instance of User" do
       VCR.use_cassette("initialize_user") do
         expect(@user).must_be_kind_of User
@@ -32,42 +32,52 @@ describe "User" do
       end
     end
   end
-end
-
-describe "list method" do 
-  it "self.list returns an array of users" do 
-    VCR.use_cassette("list_users") do 
-      results = User.list 
-      expect(results).must_be_instance_of Array 
-      results.each do |result|
-        expect(result).must_be_instance_of User
-        
-      end 
-    end 
-  end 
   
-  it "returns accurate information" do
-    VCR.use_cassette("list_users") do 
-      results = User.list
-      
-      info = [
-        {slack_id: "USLACKBOT", name: "slackbot", real_name: "Slackbot"},
-        {slack_id: "UMURTAYP5", name:"juliabouv", real_name:"Julia Bouvier"},
-        {slack_id:"UN863PN23", name:"amalh.a97", real_name:"Amal"}
-      ]
-      
-      results.each_with_index do |result, index|
-        expect(result.slack_id).must_be_kind_of String
-        expect(result.slack_id).must_equal info[index][:slack_id]
-        
-        expect(result.name).must_be_kind_of String
-        expect(result.name).must_equal info[index][:name]
-        
-        expect(result.real_name).must_be_kind_of String
-        expect(result.real_name).must_equal info[index][:real_name]
-      end
+  describe "#details" do
+    it "must return something" do
+      expect (@user.details).wont_be_nil
+    end
+
+    it "must return accurate info" do
+      expect (@user.details).must_include "Bob"
+      expect (@user.details).must_include "1234"
+      expect (@user.details).must_include "Bobiscool"
     end
   end
-end 
-
-
+  
+  describe "list method" do 
+    it "self.list returns an array of users" do 
+      VCR.use_cassette("list_users") do 
+        results = User.list 
+        expect(results).must_be_instance_of Array 
+        results.each do |result|
+          expect(result).must_be_instance_of User
+          
+        end 
+      end 
+    end 
+    
+    it "returns accurate information" do
+      VCR.use_cassette("list_users") do 
+        results = User.list
+        
+        info = [
+          {slack_id: "USLACKBOT", name: "slackbot", real_name: "Slackbot"},
+          {slack_id: "UMURTAYP5", name:"juliabouv", real_name:"Julia Bouvier"},
+          {slack_id:"UN863PN23", name:"amalh.a97", real_name:"Amal"}
+        ]
+        
+        results.each_with_index do |result, index|
+          expect(result.slack_id).must_be_kind_of String
+          expect(result.slack_id).must_equal info[index][:slack_id]
+          
+          expect(result.name).must_be_kind_of String
+          expect(result.name).must_equal info[index][:name]
+          
+          expect(result.real_name).must_be_kind_of String
+          expect(result.real_name).must_equal info[index][:real_name]
+        end
+      end
+    end
+  end 
+end
