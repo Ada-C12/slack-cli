c98a559707dc42ac9f6fa5b6821b2c98b346be48
Eve Le <nguyenthao.lee@gmail.com>
2019-09-11 14:36:00 -0700

    Implement find_recipient method for Workspace class

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -3,13 +3,13 @@ require_relative 'user'
 
 class Workspace
   attr_reader :channels, :users, :selected
-
+  
   def initialize
     @channels = Channel.list
     @users = User.list
     @selected = nil
   end
-
+  
   def show_details *recipients
     recipients = [:channels, :users] if recipients.empty?
     
@@ -17,5 +17,13 @@ class Workspace
       puts recipient.capitalize
       puts self.send(recipient).map(&:details).join("\n")
     end
+  end
+  
+  def find_recipient(list:, name: nil, slack_id: nil)
+    raise ArgumentError unless name || slack_id
+
+    return list.find do |recipient| 
+      name ? recipient.name == name : recipient.slack_id == slack_id  
+    end 
   end
 end
--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -11,19 +11,19 @@ describe Workspace do
     it 'can create a Workspace instance' do
       expect(@workspace).must_be_instance_of Workspace
     end
-
+    
     it 'has users, channels and selected' do
       expect(@workspace.channels).must_be_instance_of Array
       expect(@workspace.users).must_be_instance_of Array
       assert_nil(@workspace.selected)
     end
-
+    
     it 'populates users and channels' do
       expect(@workspace.channels.first).must_be_instance_of Channel
       expect(@workspace.users.first).must_be_instance_of User
     end
   end
-
+  
   describe '#select_channel' do
     
   end
@@ -31,8 +31,52 @@ describe Workspace do
   describe '#select_user' do
     
   end
-
+  
   describe '#send_message' do
     
+  end
+  
+  describe "#find_recipient" do
+    before do
+      @user_name = "calopter"
+      @user_id = "UN8G3G4VC"
+      @users = @workspace.users
+
+      @channel_name = "random"
+      @channel_id = "CMURJLBUK"
+      @channels = @workspace.channels
+    end
+    it "returns the correct User by slack id or name" do
+      user = @workspace.find_recipient(list: @users, name: @user_name)
+      
+      expect (user).must_be_instance_of User
+      expect (user.slack_id).must_equal @user_id
+
+      user = @workspace.find_recipient(list: @users, slack_id: @user_id)
+      
+      expect (user).must_be_instance_of User
+      expect (user.name).must_equal @user_name
+    end
+    
+    it "returns the correct Channel by slack id or name" do
+      channel = @workspace.find_recipient(list: @channels, name: @channel_name)
+      
+      expect (channel).must_be_instance_of Channel
+      expect (channel.slack_id).must_equal @channel_id
+
+      channel = @workspace.find_recipient(list: @channels, slack_id: @channel_id)
+      
+      expect (channel).must_be_instance_of Channel
+      expect (channel.name).must_equal @channel_name
+    end
+
+    it "raises ArgumentError if both name and slack id aren't provided" do
+      expect {@workspace.find_recipient(list: [])}.must_raise ArgumentError
+    end
+    
+    it "returns nil if no recipient found from name or slack id" do
+      assert_nil (@workspace.find_recipient(list: @channels, name: "Tofu"))
+      assert_nil (@workspace.find_recipient(list: @users, slack_id: "Tofu"))
+    end
   end
 end
