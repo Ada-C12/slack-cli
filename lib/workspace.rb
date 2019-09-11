7e548c6d02e5aa2f2464414a8963c64054d148a8
gracexinran <gracexinran@gmail.com>
2019-09-10 17:16:09 -0700

    defined class Slack::Workspace, created unit test file

---
+++ lib/workspace.rb
@@ -0,0 +1,17 @@
+require_relative 'channel'
+require_relative 'user'
+require_relative 'slack_api_error'
+
+module Slack
+
+  class Workspace
+    
+    def initialize
+      
+    end
+    
+    
+    
+  end
+  
+end
---
+++ test/workspace_test.rb
@@ -0,0 +1,5 @@
+require_relative 'test_helper'
+
+describe 'class workspace' do
+
+end
