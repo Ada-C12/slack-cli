84b14fdc4a419b33a602f3dc0d6f25731f54b483
geli-gel <angele.zam@gmail.com>
2019-09-11 15:00:58 -0700

    created CLI and menu_options method

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,11 +1,51 @@
 #!/usr/bin/env ruby
+require_relative 'workspace'
+require 'table_print'
+require 'terminal-table'
 
 def main
-  puts "Welcome to the Ada Slack CLI!"
+  puts "\nWelcome to the Ada Slack CLI!"
+  workspace = SlackCLI::Workspace.new
+  channels_count = workspace.channels.length
+  users_count = workspace.users.length
+  puts "\n#{channels_count} channels and #{users_count} users loaded"
+  
+  choice = ""
+  until choice == "quit" || choice == "3"
+    puts "\nWhat would you like to do?"
+    menu_options
+    print "Action: " 
+    choice = gets.chomp.downcase
+    case choice
+    when "1","list users"
+      list_users
+    when "2","list channels"
+      list_channels
+    else 
+      puts "That option does not exist"
+    end
+  end
 
-  # TODO project
-
-  puts "Tharnk you for using the Ada Slack CLI"
+  puts "Thank you for using the Ada Slack CLI"
 end
+
+def menu_options
+  numbered_options = [
+    ["1", "list users"], 
+    ["2", "list channels"], 
+    ["3", "quit"]
+  ]
+  menu_table = Terminal::Table.new :headings => ["#", "Action"], :rows => numbered_options
+  puts menu_table
+end
+
+def list_users
+  # list users
+end
+
+def list_channels
+  # list channels
+end
+
 
 main if __FILE__ == $PROGRAM_NAME
