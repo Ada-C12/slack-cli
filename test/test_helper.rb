cef385ac5885aeafd464335034059ac5b17a30f4
Julia Bouvier <juliabouv@gmail.com>
2019-09-10 17:08:23 -0700

    Updated requires

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,14 +2,21 @@ require 'simplecov'
 SimpleCov.start do
   add_filter 'test/'
 end
+require "dotenv"
+Dotenv.load
 
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require 'vcr'
-require "dotenv"
-Dotenv.load
+require 'webmock/minitest'
+require 'httparty'
+
+require_relative '../lib/slack_api_error'
+require_relative '../lib/recipient'
+require_relative '../lib/channel'
+require_relative '../lib/user'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
@@ -18,8 +25,8 @@ VCR.configure do |config|
   config.hook_into :webmock
   
   # Don't leave our token lying around in a cassette file.
-  config.filter_sensitive_data("<SLACK_API_TOKEN>") do
-    ENV["SLACK_API_TOKEN"]
+  config.filter_sensitive_data("<SLACK_TOKEN>") do
+    ENV["SLACK_TOKEN"]
   end
 end
 
