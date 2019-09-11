dd51b5cdbb0f0617661d5e8029caecd9b9080e39
Farah Davoodi <farahdavoodi@gmail.com>
2019-09-11 15:15:50 -0700

    deleted cassettes and added filter sensitive data for token

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,6 +1,11 @@
+require 'httparty'
+require 'pry'
+
 class Channel < Recipient
   attr_reader :slack_id, :name, :topic, :member_count
   @@channels_list = []
+  BASE_URL = "https://slack.com/api/channels.list"
+  API_KEY = ENV['SLACK_API_TOKEN']
 
   def initialize(slack_id, name, topic, member_count)
     super(slack_id, name)
@@ -10,7 +15,14 @@ class Channel < Recipient
   end
 
   def self.list
-    return @@channels_list
+    query = {
+      token: API_KEY
+    }
+
+    response = HTTParty.get(BASE_URL, query: query)
+    binding.pry
+
+    return response
   end
 end
 
--- test/channel_test.rb
+++ test/channel_test.rb
@@ -1,29 +1,27 @@
 require_relative 'test_helper'
 
 describe "Channel Class"do
-  describe "constructor initialize" do
-    it "should return an instance of channel" do
-      slack_id = 4
-      name = "Sick Rides"
-      topic = "Post here about your coolest cars"
-      member_count = 2
+  # describe "constructor initialize" do
+    # it "should return an instance of channel" do
+    #   slack_id = 4
+    #   name = "Sick Rides"
+    #   topic = "Post here about your coolest cars"
+    #   member_count = 2
 
-      slack_channel = Channel.new(slack_id, name, topic, member_count)
-      expect(slack_channel.slack_id).must_equal slack_id
-      expect(slack_channel.name).must_equal name 
-      expect(slack_channel.topic).must_equal topic
-      expect(slack_channel.member_count).must_equal member_count
-    end
-  end
+    #   slack_channel = Channel.new(slack_id, name, topic, member_count)
+    #   expect(slack_channel.slack_id).must_equal slack_id
+    #   expect(slack_channel.name).must_equal name 
+    #   expect(slack_channel.topic).must_equal topic
+    #   expect(slack_channel.member_count).must_equal member_count
+    # end
+  # end
 
   describe "self.list method" do
-    it "should return an array" do
-      slack_id = 4
-      name = "Sick Rides"
-      topic = "Post here about your coolest cars"
-      member_count = 2
-      slack_channel = Channel.new(slack_id, name, topic, member_count)
-      expect(Channel.list).must_be_kind_of Array
+    it "should return true if the list was returned" do
+      VCR.use_cassette("channel-tests") do
+        response = Channel.list
+        expect(response).must_equal true
+      end
     end
   end
 end
