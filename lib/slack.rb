74e0a9ee2eb261625cdd5fd1bcee477fbc846d97
Samantha Coll <samcoll@Samanthas-MacBook-Pro.local>
2019-09-10 19:14:19 -0700

    updated slack.rb to acess slack workspace

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,11 +1,21 @@
+require "dotenv"
+require "httparty"
+
+Dotenv.load
+
 #!/usr/bin/env ruby
 
-def main
-  puts "Welcome to the Ada Slack CLI!"
+# def main
+#   puts "Welcome to the Ada Slack CLI!"
 
-  # TODO project
+#   # TODO project
 
-  puts "Thank you for using the Ada Slack CLI"
-end
+#   puts "Thank you for using the Ada Slack CLI"
+# end
 
-main if __FILE__ == $PROGRAM_NAME
+# main if __FILE__ == $PROGRAM_NAME
+
+# Accessing my workspace
+URL = "https://slack.com/api/users.list"
+response = HTTParty.get(URL, query: {token: ENV['SLACK_API_TOKEN']})
+#p response
