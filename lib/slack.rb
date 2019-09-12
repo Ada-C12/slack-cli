ce610fee72695b9a914a33ea4aa85c285754eb38
Janice Huang <janicehuang@gmail.com>
2019-09-11 22:48:06 -0700

    add select user and select channel functions

--- lib/slack.rb
+++ lib/slack.rb
@@ -9,25 +9,38 @@ Dotenv.load
 def main
   puts "Welcome to the Ada Slack CLI!"
   tsu = SlackCLI::Workspace.new
-  
-  loop do 
+
+  loop do
     print "Would you like to (1) list users, (2) list channels, or (3) quit? "
     choice = gets.chomp.to_i
     until [1, 2, 3].include?(choice)
-      print "Sorry. Please enter '1', '2' or '3'!"
-      choice = gets.chomp
+      print "Sorry. Please enter '1', '2' or '3'! "
+      choice = gets.chomp.to_i
     end
-    
+
     if choice == 1
       tsu.list_users
+      print "Please select a user (by Slack ID or Display Name): "
+      user_chosen = gets.chomp
+      if [tsu.user_menu].include?(user_chosen)
+        tsu.print_details(user_chosen)
+      else
+        print "Sorry. Invalid selection."
+      end
     elsif choice == 2
       tsu.list_channels
+      print "Please select a channel (by Slack ID or Name): "
+      channel_chosen = gets.chomp
+      if [tsu.channel_menu].include?(channel_chosen)
+        tsu.print_details(channel_chosen)
+      else
+        print "Sorry. Invalid selection."
+      end
     elsif choice == 3
       puts "Thank you for using the Ada Slack CLI"
       exit
     end
-    
-  end 
+  end
 end
 
 main
--- lib/workspace.rb
+++ lib/workspace.rb
@@ -10,48 +10,70 @@ Dotenv.load
 module SlackCLI
   class Workspace
     attr_accessor :users, :channels, :selected
-    
+
     CHANNEL_URL = 'https://slack.com/api/channels.list'
     USER_URL = 'https://slack.com/api/users.list'
     GET_PARAMETERS = {
       token: ENV['SLACK_KEY']
     }
-    
+
     def initialize
       @users = SlackCLI::User.json_parse(SlackCLI::User.get(USER_URL, query: GET_PARAMETERS))
       @channels = SlackCLI::Channel.json_parse(SlackCLI::Channel.get(CHANNEL_URL, query: GET_PARAMETERS))
       @selected = nil
-      
+
       puts "Workspace loaded! #{users.length} users and #{channels.length} channels available."
     end
-    
+
     def list_users
       users_hash_array = []
       @users.each do |user|
         user_hash = {
           "slack_id" => user.slack_id,
-          "display name" => user.name, 
-          "real name" => user.real_name, 
+          "display name" => user.name,
+          "real name" => user.real_name,
           "status text" => user.status_text,
           "status emoji" => user.status_emoji
         }
         users_hash_array << user_hash
-      end 
-      tp users_hash_array 
+      end
+      tp users_hash_array
     end
-    
+
+    def user_menu
+      user_menu_array = []
+      @users.each do |user|
+        user_menu_array << user.slack_id
+        user_menu_array << user.name
+      end
+      return user_menu_array
+    end
+
     def list_channels
       channels_hash_array = []
       @channels.each do |channel|
         channel_hash = {
           "slack_id" => channel.slack_id,
-          "name" => channel.name, 
+          "name" => channel.name,
           "topic" => channel.topic,
           "member_count" => channel.member_count
         }
         channels_hash_array << channel_hash
-      end 
-      tp channels_hash_array 
+      end
+      tp channels_hash_array
     end
-  end 
+
+    def channel_menu
+      channel_menu_array = []
+      @channels.each do |channel|
+        channel_menu_array << channel.slack_id
+        channel_menu_array << channel.name
+      end
+      return channel_menu_array
+    end
+
+    def print_details(selected)
+      
+    end
+  end
 end
