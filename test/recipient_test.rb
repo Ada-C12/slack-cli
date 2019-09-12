c48ec49a3f50fa1e735c7e6bbe30fd23b07bd5b7
gracexinran <gracexinran@gmail.com>
2019-09-12 13:50:33 -0700

    Created unit tests for self.get, self.list and details methods

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -3,8 +3,7 @@ require_relative 'test_helper'
 describe 'class recipient' do
   describe 'initialize' do 
     let(:recipient){
-      Slack::Recipient.new(id: "C0G9QF9GW", name: "Xinran")
-    }
+    Slack::Recipient.new(id: "C0G9QF9GW", name: "Hey")}
     
     it 'creates a new instance of Recipient' do
       expect(recipient).must_be_instance_of Slack::Recipient
@@ -17,9 +16,47 @@ describe 'class recipient' do
     
     it 'accurately stores name' do
       expect(recipient.name).must_be_instance_of String
-      expect(recipient.name).must_equal "Xinran"
+      expect(recipient.name).must_equal "Hey"
+    end
+  end
+  
+  describe 'self.get' do
+    
+    
+    it 'should return an instance of HTTParty::Response' do
+      VCR.use_cassette("recipient_get_method") do
+        url = "https://slack.com/api/users.list"
+        params = {token: ENV["SLACK_TOKEN"]}
+        
+        response = Slack::Recipient.get(url, params)
+        
+        expect(response).must_be_instance_of HTTParty::Response
+      end
+      
     end
     
-    
+    it 'should raise a Slack_Api_Error if the response code is not 200' do
+      VCR.use_cassette("get_recipient_error") do
+        puts 
+        expect{Slack::Recipient.get("https://us1.locationiq.org/v1/search.php?", {token: ENV["SLACK_TOKEN"]})}.must_raise Slack::Slack_Api_Error
+        
+        expect{Slack::Recipient.get("https://slack.com/api/users.list", {token: "XXXXXXXX"})}.must_raise Slack::Slack_Api_Error
+        
+      end
+    end
   end
+  
+  describe "self.list" do
+    it "must raise NotImplementedError" do 
+      expect{Slack::Recipient.list}.must_raise NotImplementedError
+    end
+  end
+  
+  describe "details" do
+    it "must raise NotImplementedError" do
+      tester = Slack::Recipient.new(name: "Xinran" , id: "1234")
+      expect{tester.details}.must_raise NotImplementedError
+    end
+  end
+  
 end
