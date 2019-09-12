8b98e352129489543d3d71a581cc1d8b04eb558d
Brianna Kemp <kemp.bri@gmail.com>
2019-09-12 14:58:07 -0700

    Implemented wave 2 methods and tests, driver code still needed

--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -54,6 +54,16 @@ describe "Workspace" do
         expect(workspace.selected.slack_id).must_equal "UMUR2C1QB"
       end
     end
+    
+    it "returns message if user is not found" do
+      VCR.use_cassette("create_workspace") do
+        workspace = Workspace.new
+        result = workspace.select_user("aerstdhsg")
+        
+        expect(result).must_be_kind_of String
+        expect(result.length).must_equal 19
+      end
+    end
   end
   
   describe "select channel" do
@@ -76,6 +86,51 @@ describe "Workspace" do
         expect(workspace.selected).must_be_kind_of Channel
         expect(workspace.selected.name).must_equal "general"
         expect(workspace.selected.slack_id).must_equal "CMTFYSFTL"
+      end
+    end
+    
+    it "returns message if channel is not found" do
+      VCR.use_cassette("create_workspace") do
+        workspace = Workspace.new
+        result = workspace.select_channel("hgfdghf")
+        
+        expect(result).must_be_kind_of String
+        expect(result.length).must_equal 22
+      end
+    end
+  end
+  
+  describe "show details" do
+    it "shows details for selected user" do
+      VCR.use_cassette("create_workspace") do
+        workspace = Workspace.new
+        workspace.select_user("kemp.bri")
+        details = workspace.show_details
+        
+        expect(details).must_be_kind_of String
+        expect(details.length).must_equal 103
+      end
+    end
+    
+    it "shows details for selected channel" do
+      VCR.use_cassette("create_workspace") do
+        workspace = Workspace.new
+        workspace.select_channel("general")
+        details = workspace.show_details
+        
+        expect(details).must_be_kind_of String
+        expect(details.length).must_equal 126
+      end
+    end
+    
+    it "returns message for non-selected user/channel" do
+      VCR.use_cassette("create_workspace") do
+        workspace = Workspace.new
+        workspace.select_channel("sshtirl")
+        details = workspace.show_details
+        
+        expect(details).must_be_kind_of String
+        expect(details.length).must_equal 32
       end
     end
   end
