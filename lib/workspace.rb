6c789bca15f7b6b379fce64e022bd00b92ca88e6
Julia Bouvier <juliabouv@gmail.com>
2019-09-11 21:59:09 -0700

    Implemented #select_channel, #select_user and #show_details tests

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -22,7 +22,7 @@ class Workspace
   def select_channel(input: nil)
     channel_names = @channels.map { |channel| channel.name }
     channel_ids = @channels.map { |channel| channel.slack_id }
-
+    
     if channel_names.include?(input)
       @selected = @channels.find { |c| input == c.name }
     elsif channel_ids.include?(input)
@@ -36,13 +36,13 @@ class Workspace
   def select_user(input: nil)
     usernames = @users.map { |user| user.name }
     user_ids = @users.map { |user| user.slack_id }
-
+    
     if usernames.include?(input)
       @selected = @users.find { |u| input == u.name }
     elsif user_ids.include?(input)
       @selected = @users.find { |u| input == u.slack_id }
     else
-      raise ArgumentError.new "Argument must include name or id"
+      raise ArgumentError.new "Argument must include valid name or id"
     end
     return @selected
   end
