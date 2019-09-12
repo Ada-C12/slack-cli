a6f7fd4ff0c28eb03c3c4d94fab4c98c400b2e3a
daniela sanchez <dnsanche@gmail.com>
2019-09-12 15:02:07 -0700

    Updates to slack.rb and workspace.rb

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,21 +1,41 @@
 #lib/slack.rb
 require "httparty"
 require "awesome_print"
+require "colorize"
+require "table_print"
 require_relative "workspace"
 
 def main
-  puts "Welcome to the Ada Slack CLI!"
-  @workspace = Slack::Workspace.new() 
-  @workspace.users
-  @workspace.channels
-  @workspace.select_user("UMTERDBPU")
-  # @workspace.select_channel("CMUPUL1R9")
-  # p @workspace.show_details
-  
-  
-  # TODO project
-
-  puts "Thank you for using the Ada Slack CLI"
+ puts "Welcome to the Ada Slack CLI!".colorize(:color => :blue, :mode => :bold)
+ @workspace = Slack::Workspace.new()
+ puts "\nPlease Choose from the following options:
+ \n1. List Users
+ \n2. List Channels
+ \n3. Select User
+ \n4. Select Channel
+ \n5. Details
+ \n6. Send Message
+ \n7. Quit".colorize(:color => :blue, :mode => :bold)
+ option = gets.chomp.downcase
+ while option != "quit"
+   if option == "list users"
+     puts @workspace.users
+   elsif option == "list channels"
+     return @workspace.channels
+   elsif option == "select user"
+     return @workspace.select_user(option)
+   elsif option == "select channel"
+     return @workspace.select_channel(option)
+   elsif option == "details"
+     return @workspace.show_details
+   elsif main
+   else
+     option == "quit"
+     puts "Good Bye"
+     exit
+   end
+ end
+ puts "Thank you for using the Ada Slack CLI"
 end
 
 main if __FILE__ == $PROGRAM_NAME
