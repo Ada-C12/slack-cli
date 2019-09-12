f4ee658ae2e0cfb69530cc2692427553c5f38b45
brilatimer <brihiramine@gmail.com>
2019-09-12 12:01:35 -0700

    wave 2 logic steps

--- lib/slack.rb
+++ lib/slack.rb
@@ -18,7 +18,7 @@ def main
   users = Slack::User.list_users
   channels = Slack::Channel.list_channels
   puts "Welcome to the Ada Slack CLI!"
-  puts "There are #{users.length} users and #{channels.length}."
+  puts "There are #{users.length} users and #{channels.length} channels."
   
   puts "Would you like to view: List Users, List Channels, or Quit?" # add number options?
   user_input = gets.chomp.split.map(&:capitalize).join(' ')
@@ -32,23 +32,34 @@ when user_input == List Channels
   # Wave 2: additional options 
   puts "Please select user."
   # by entering username or slack ID
-  
-  puts "Please select channel."
-  # by entering channel name or slack ID
-  
-  puts "Here are the details of your selection."
-  # if user_input is not vaild, reprint list options
-  # if channel, puts channel name or slack ID
-  # if user, puts username or slack ID
-  
-  # if no user/channel exists with selection, return to initial options list
-  # if no selected recepient, return to initial options
-  
-  while user_input != "List Users" && user_input != "List Channels" && user_input != "Quit"
-    puts "\nIncorrect input!! Please select: List Users, List Channels, or Quit"
-    user_input = gets.chomp.split.map(&:capitalize).join(' ')
-  end
-  puts "Thank you for using the Ada Slack CLI"
-end
-
-main if __FILE__ == $PROGRAM_NAME
+  if user_input == user_name || slack_id
+    # go to next step
+    # if no user/channel exists with selection, return to initial options list
+  elsif user_input != user_name || slack_id
+    puts "Please select user."
+    
+    puts "Please select channel."
+    # by entering channel name or slack ID
+    if user_input == channel || slack_id
+      # go to next step
+      
+      puts "Here are the details of your selection."
+      # if user_input is not vaild, reprint list options
+      
+      # if user_input == channel
+      # puts #{channel_name} or #{slack ID}
+      
+      # if user_input == user
+      # puts user_name || slack_id
+      
+      # if no selected recepient, return to initial options
+      
+      while user_input != "List Users" && user_input != "List Channels" && user_input != "Quit"
+        puts "\nIncorrect input!! Please select: List Users, List Channels, or Quit"
+        user_input = gets.chomp.split.map(&:capitalize).join(' ')
+      end
+      puts "Thank you for using the Ada Slack CLI"
+    end
+    
+    main if __FILE__ == $PROGRAM_NAME
+    
