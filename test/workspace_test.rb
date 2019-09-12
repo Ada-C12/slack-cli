76f7c949ebdb8e1f90e0544d1d2c4b2d3dd7434b
Julia Bouvier <juliabouv@gmail.com>
2019-09-11 21:59:09 -0700

    Implemented #select_channel, #select_user and #show_details tests

--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -32,6 +32,59 @@ describe "Workspace" do
     end
   end
   
+  describe "#select_channel" do
+    it "returns instance of Channel when name is provided" do
+      expect(@workspace.select_channel(input: "random")).must_be_instance_of Channel
+    end
+
+    it "returns instance of Channel when id is provided" do
+      expect(@workspace.select_channel(input: "CN5RT17J8")).must_be_instance_of Channel
+    end
+
+    it "raises exception if no name or id provided" do
+      expect{ @workspace.select_channel }.must_raise ArgumentError
+    end
+  end
+  
+  describe "#select_user" do
+    it "returns instance of User when name is provided" do
+      expect(@workspace.select_user(input: "slackbot")).must_be_instance_of User
+    end
+
+    it "returns instance of User when id is provided" do
+      expect(@workspace.select_user(input: "USLACKBOT")).must_be_instance_of User
+    end
+
+    it "raises exception if no name or id provided" do
+      expect{ @workspace.select_user }.must_raise ArgumentError
+    end
+  end
+
+  describe "#show_details" do
+    it "returns details if @selected is user" do
+      @workspace.select_user(input: "slackbot")
+
+      expect(@workspace.show_details).wont_be_nil
+      expect(@workspace.show_details).must_include "slackbot"
+      expect(@workspace.show_details).must_include "Slackbot"
+      expect(@workspace.show_details).must_include "USLACKBOT"
+    end
+
+    it "returns details if @selected is channel" do
+      expect(@workspace.select_channel(input: "random"))
+
+      expect(@workspace.show_details).wont_be_nil
+      expect(@workspace.show_details).must_include "random"
+      expect(@workspace.show_details).must_include "CN5RT17J8"
+      expect(@workspace.show_details).must_include "2"
+      expect(@workspace.show_details).must_include "Non-work banter and water cooler conversation"
+    end
+
+    it "returns nil if nothing is @selected" do
+      expect(@workspace.show_details).must_be_nil
+    end
+  end
+  
   xdescribe "#send_message" do
     it "can send a valid message" do
       response = @workspace.send_message("This is a message", "general")
