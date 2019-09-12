36412bd5e122145a6430aa2df0ec635abb9d2223
Julia Bouvier <juliabouv@gmail.com>
2019-09-11 21:57:54 -0700

    Implemented #select_user and #select_channel methods

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -19,16 +19,40 @@ class Workspace
     @selected = nil
   end
   
-  def select_channel
+  def select_channel(input: nil)
+    channel_names = @channels.map { |channel| channel.name }
+    channel_ids = @channels.map { |channel| channel.slack_id }
+
+    if channel_names.include?(input)
+      @selected = @channels.find { |c| input == c.name }
+    elsif channel_ids.include?(input)
+      @selected = @channels.find { |c| input == c.slack_id }
+    else
+      raise ArgumentError.new "Argument must include valid name or id"
+    end
+    return @selected
   end
   
-  def select_user
+  def select_user(input: nil)
+    usernames = @users.map { |user| user.name }
+    user_ids = @users.map { |user| user.slack_id }
 
+    if usernames.include?(input)
+      @selected = @users.find { |u| input == u.name }
+    elsif user_ids.include?(input)
+      @selected = @users.find { |u| input == u.slack_id }
+    else
+      raise ArgumentError.new "Argument must include name or id"
+    end
+    return @selected
   end
   
   def show_details
+    return @selected.details if @selected
   end
   
-  def send_message
+  def send_message(message, channel)
+    
+    # return response["ok"]
   end
 end
