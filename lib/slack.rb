0d476c0b01b806657acec401a61d7d736256d51f
Emily Vomacka <emilyvomacka@gmail.com>
2019-09-11 20:48:25 -0700

    finished command line to complete wave 1

--- lib/slack.rb
+++ lib/slack.rb
@@ -9,20 +9,25 @@ Dotenv.load
 def main
   puts "Welcome to the Ada Slack CLI!"
   tsu = SlackCLI::Workspace.new
-  print "Would you like a list of (1) users or (2) channels? "
-  choice = gets.chomp.to_i
-  until [1, 2].include?(choice)
-    print "Sorry. Please enter '1' or '2'!"
-    choice = gets.chomp
-  end
   
-  if choice == 1
-    tsu.list_users
-  elsif choice == 2
-    tsu.list_channels
-  end
-  
-  puts "Thank you for using the Ada Slack CLI"
+  loop do 
+    print "Would you like to (1) list users, (2) list channels, or (3) quit? "
+    choice = gets.chomp.to_i
+    until [1, 2, 3].include?(choice)
+      print "Sorry. Please enter '1', '2' or '3'!"
+      choice = gets.chomp
+    end
+    
+    if choice == 1
+      tsu.list_users
+    elsif choice == 2
+      tsu.list_channels
+    elsif choice == 3
+      puts "Thank you for using the Ada Slack CLI"
+      exit
+    end
+    
+  end 
 end
 
 main
