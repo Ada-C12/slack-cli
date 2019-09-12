6e24945b3a77432a6bb8b00b1af1e1bf1e030bb8
emaust <erikamaust@gmail.com>
2019-09-12 14:04:34 -0700

    User list method written, .history/ added to .gitignore

--- lib/user.rb
+++ lib/user.rb
@@ -1,25 +1,50 @@
 require_relative 'recipient'
+
+require 'httparty'
+require 'dotenv'
+require 'awesome_print'
+require 'table_print'
+require 'pry'
+require 'awesome_print'
+Dotenv.load
+
+class User < Recipient
+  attr_accessor :slack_id
   
+  URL = "https://slack.com/api/users.list"
+  KEY = ENV['SLACK_TOKEN']
   
-  class User < Recipient
-    attr_reader :real_name, :status_text, :status_emoji
-    
-    def initialize
-      super(slack_id, name)
-      @real_name = real_name
-      @status_text = status_text
-      @status_emoji = status_emoji
-    end
-    
-    def details
-      
-      
-    end
-    
-    
-    def self.list
-      
-      
-    end
+  def initialize(slack_id:, user_name:, real_name:) 
+    # super(slack_id, name)
+    @user_name = user_name
+    @real_name = real_name
+    @slack_id = slack_id
+  end
+  
+  def details
+    #print out the details of the selected recipient
     
   end
+  
+  
+  def self.list
+    response = HTTParty.get(URL, query: {token: KEY})
+    members = response["members"]
+    
+    users = []
+    
+    members.each do |member|
+      user = self.new(slack_id: member["id"], real_name: member["real_name"], user_name: member["name"])
+      users << user
+      
+      #tp and self.get
+    end
+    return users
+    # users.each do |user| 
+    #   puts use
+    # end
+  end
+  
+end
+
+p User.list
