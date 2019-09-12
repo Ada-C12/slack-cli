723089af78143f8ffbab1e3f01846f7456ce2755
Nicky Choi <nickyjinchoi@gmail.com>
2019-09-11 21:15:29 -0700

    fixed send_message method

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -15,28 +15,19 @@ module Slack
       return HTTParty.get(url, params)
     end
 
-    MESSAGE_URL = "https://slack.com/api/chat.postMessage"
+    def send_message(message)
+      body = {
+        token: ENV["SLACK_TOKEN"],
+        channel: slack_id,
+        text: message,
+      }
 
-    def post_message(message)
-      if @selected.is_a?(Slack::Channel)
-        body = {
-          token: ENV["SLACK_TOKEN"],
-          channel: slack_id,
-          text: message,
-        }
+      response = HTTParty.post(MESSAGE_URL, body: body)
 
-        response = HTTParty.post(MESSAGE_URL, body: body)
-
-        if response["ok"] != true
-          raise StandardError, "Invalid request. Error is #{response.code}: #{response.message}"
-        end
-        # else
-        #   body = {
-        #     token: ENV["SLACK_TOKEN"],
-        #     channel: slack_id,
-        #     text: message,
-        #   }
+      if response["ok"] != true
+        raise StandardError, "Invalid request. Error is #{response.code}: #{response.message}"
       end
+      return response
     end
 
     private
