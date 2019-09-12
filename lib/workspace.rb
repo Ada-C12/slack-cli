5c8f70bd207003bcabb5ed6dee72a1df4c9042c9
Georgina Sanchez Buenrostro <cpgeorgina.sanchez@gmail.com>
2019-09-12 16:29:48 -0700

    raises an argument error for invalid selection

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,4 +1,5 @@
 require 'dotenv'
+require "pry"
 Dotenv.load
 
 class Workspace
@@ -7,4 +8,19 @@ class Workspace
     @channels = channels
     @selected = selected 
   end
+
+  def select_channel(selection)
+    value = 0
+
+    Channel.list.each do |channel|
+      if channel.name == selection || channel.slack_id == selection
+        value = 1
+        return channel
+      end
+    end
+
+    if value = 0
+      raise ArgumentError.new("That is wrong") 
+    end
+  end 
 end
--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -19,4 +19,22 @@ describe "Workspace" do
         expect(@workspace).must_be_kind_of Workspace
     end
   end
+
+  describe "select_channel" do
+    it "returns a selected channel instance" do
+      VCR.use_cassette("workspace") do
+        #Act
+        selected_channel = @workspace.select_channel("random")
+
+        #Assert
+        expect(selected_channel).must_be_instance_of Channel
+      end
+    end
+
+    it "raises an argument error for invalid selection" do
+      VCR.use_cassette("workspace") do
+        expect {(@workspace.select_channel("Pizza"))}.must_raise ArgumentError
+      end
+    end
+  end    
 end
