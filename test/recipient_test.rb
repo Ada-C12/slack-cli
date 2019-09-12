7b0020d11015e8a8c7f314c6e0272f6ebde72e17
stupendousC <CarolineWuKaplan@gmail.com>
2019-09-12 16:44:58 -0700

    passed w/ 100% coverage

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -1,1 +1,19 @@
 require_relative 'test_helper.rb'
+
+describe "##### RECIPIENT TEST #######" do
+
+  it "does Recipient.details work?" do
+    VCR.use_cassette("WT1") do
+      recip1 = Recipient.new(id: "fake", name: "Fakey McFake")
+      expect{recip1.details}.must_raise NotImplementedError
+    end
+  end
+
+  it 'load_all work?' do
+    VCR.use_cassette("RT1") do
+      expect{Recipient.load_all}.must_raise NotImplementedError
+    end
+  end
+
+end
+
