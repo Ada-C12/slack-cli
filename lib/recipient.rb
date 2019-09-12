4e89c87ba0e251ec71d650cc1411a2e5820d130e
gracexinran <gracexinran@gmail.com>
2019-09-12 13:49:34 -0700

    Defined self.get method

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,3 +1,5 @@
+require_relative 'slack_api_error'
+
 module Slack
   class Recipient
     
@@ -8,6 +10,23 @@ module Slack
       @id = id
     end
     
+    def self.get(url, params)
+      response = HTTParty.get(url, query: params)
+      
+      if response.code != 200 || response['ok'] == false
+        raise Slack_Api_Error.new "Error! No response found."
+      end
+      
+      return response
+    end
+    
+    def self.list
+      raise NotImplementedError, 'Implement me in a child class!'
+    end
+    
+    def details
+      raise NotImplementedError, 'Implement me in a child class!'
+    end
     
   end
   
