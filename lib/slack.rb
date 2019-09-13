51c83c45919d0946da7ecd4466779161345e55c1
geli-gel <angele.zam@gmail.com>
2019-09-13 11:56:43 -0700

    added select_user, select_channel, and send_message to slack.rb

--- lib/slack.rb
+++ lib/slack.rb
@@ -11,7 +11,7 @@ def main
   puts "\n#{channels_count} channels and #{users_count} users loaded"
   
   choice = ""
-  until choice == "3" || choice == "quit"
+  until choice == "7" || choice == "quit"
     puts "\nWhat would you like to do?"
     menu_options
     print "Action: " 
@@ -20,9 +20,23 @@ def main
     case choice
     when "1","list users"
       list_users(workspace)
+
     when "2","list channels"
       list_channels(workspace)
-    when "6","quit"
+
+    when "3","select user"
+      select_user(workspace)
+
+    when "4","select channel"
+      select_channel(workspace)
+
+    when "5","details"
+      details(workspace)
+
+    when "6","send message"
+      send_message(workspace)
+
+    when "7","quit"
       break
     else 
       puts "That option does not exist"
@@ -39,7 +53,8 @@ def menu_options
     ["3", "select user"],
     ["4", "select channel"],
     ["5", "details"],
-    ["6","quit"]
+    ["6", "details"],    
+    ["7","quit"]
   ]
   menu_table = Terminal::Table.new :headings => ["#", "Action"], :rows => numbered_options
   puts menu_table
@@ -53,5 +68,48 @@ def list_channels(workspace)
   tp workspace.channels, :slack_id, :name, :topic, :member_count
 end
 
+def select_user(workspace)
+  puts "Select User"
+  print "Please enter a Username or Slack ID: "
+  query = gets.chomp.downcase
+  query_result = workspace.select_user(query)
+  if query_result == nil
+    puts "\nUser with Username or Slack ID '#{query}' does not exist."
+  else
+    puts "\nUser '#{query_result.name}' selected"
+  end
+  return query_result
+end
+
+def select_channel(workspace)
+  puts "Select Channel"
+  print "Please enter a Channel Name or Slack ID: "
+  query = gets.chomp.downcase
+  query_result = workspace.select_channel(query)
+  if query_result == nil
+    puts "\nChannel with Name or Slack ID '#{query}' does not exist."
+  else
+    puts "\nChannel '#{query_result.name}' selected"
+  end
+  return query_result
+end
+
+def send_message(workspace)
+  if workspace.selected == nil
+    puts "\nNo recipient selected to send message to."
+    return nil
+  else
+    recipient = workspace.selected
+    puts "Send Message to '#{recipient.name}'"
+    print "Please enter message text: "
+    message_text = gets.chomp
+    
+    message_sent = recipient.send_message(message_text, recipient.slack_id)
+    
+    puts "Message Successfully Sent"
+    end
+  end
+
+end
 
 main if __FILE__ == $PROGRAM_NAME
