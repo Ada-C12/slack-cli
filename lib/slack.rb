18ba315457b3a7244369dcfb787c47dc3684d8b6
brilatimer <brihiramine@gmail.com>
2019-09-13 08:07:36 -0700

    select channel method complete and if no recepient match

--- lib/slack.rb
+++ lib/slack.rb
@@ -7,9 +7,6 @@ require_relative './channel'
 Dotenv.load('../.env')
 
 KEY = ENV['KEY']
-
-# CHANNELS_URL = "https://slack.com/api/channels.list"
-#  USERS_LIST = "https://slack.com/api/users.list"
 #CHAT_URL = "https://slack.com/api/chat.postMessage"
 
 def main
@@ -21,6 +18,7 @@ def main
   puts "Welcome to the Ada Slack CLI!"
   puts "There are #{users.length} users and #{channels.length} channels."
   
+  # Loop
   while true
     puts "Would you like to view: List Users, List Channels, Select User, Select Channel, See Details, or Quit?" 
     user_input = gets.chomp.split.map(&:capitalize).join(' ')
@@ -34,16 +32,30 @@ def main
         puts "Channel's name: #{channel.channel_name}, Member Count: #{channel.member_count}, and Slack ID: #{channel.slack_id}."
       end
       
-      # Wave 2 
+      # Wave 2: Selection Method
     when "Select User"
       puts "Please enter a username or slack id to continue."
       recepient_selection = gets.chomp
       recepient = users.find {| user | user.user_name == recepient_selection || user.slack_id == recepient_selection }
+      if recepient == nil
+        puts "No username or slack id found. Returning you to main menu."
+      end
       
     when "Select Channel"
       puts "Please enter a channel name or slack id to continue."
+      recepient_selection = gets.chomp
+      recepient = channels.find {| channel | channel.channel_name == recepient_selection || channel.slack_id == recepient_selection }
+      if recepient == nil
+        puts "No channel name or slack id found. Returning you to main menu."
+      end
+      
     when "See Details"
-      puts  recepient.details 
+      if recepient == nil # if no recepient is selected, no details can be shown
+        puts "No recepient selected. Returning you to main menu."
+      else 
+        puts recepient.details # if recepient selected, show details
+      end
+      
     when "Quit"
       puts "Exiting program"
       quit
@@ -52,35 +64,8 @@ def main
     end
   end
   
-  # Wave 2: additional options 
-  puts "Please select user."
-  # by entering username or slack ID
-  if user_input == user_name || slack_id
-    # go to next step
-    # if no user/channel exists with selection, return to initial options list
-  elsif user_input != user_name || slack_id
-    puts "Please select user."
-    
-    puts "Please select channel."
-    # by entering channel name or slack ID
-    if user_input == channel || slack_id
-      # go to next step
-      
-      puts "Here are the details of your selection."
-      # if user_input is not vaild, reprint list options
-      
-      # if user_input == channel
-      # puts #{channel_name} or #{slack ID}
-      
-      # if user_input == user
-      # puts user_name || slack_id
-      
-      # if no selected recepient, return to initial options
-      
-      
-      puts "Thank you for using the Ada Slack CLI"
-    end
-  end
+  # Program ends and exits
+  puts "Thank you for using the Ada Slack CLI"
 end
 
 # wave 3 
