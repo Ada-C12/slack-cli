3588db00f55031c12c3545536e42d4cbf86c5809
Paige Davenport <davenport.paige@outlook.com>
2019-09-13 11:57:49 -0700

    Added details functionality to slack.rb

--- lib/slack.rb
+++ lib/slack.rb
@@ -15,20 +15,35 @@ def main
     puts "\nWhat would you like to do?"
     menu_options
     print "Action: " 
+    #Make an "ask for input" helper method?
     choice = gets.chomp.downcase
     
     case choice
     when "1","list users"
+      puts "\n"
       list_users(workspace)
     when "2","list channels"
+      puts "\n"
       list_channels(workspace)
-    when "6","quit"
+    when "5","details"
+      if workspace.show_details == nil
+        puts "\nNo user or channel selected."
+      else
+        puts "#{workspace.show_details}"
+      end 
+    # when "6","send message"
+    # if workspace.selected != nil
+    # else "No user or channel (recipient) selected"
+    # should confirm channel/user selected?
+    # should do something if no channel/user selected
+      
+    when "6","quit" #change to option 7
       break
     else 
       puts "That option does not exist"
     end
   end
-
+  
   puts "Thank you for using the Ada Slack CLI"
 end
 
@@ -39,7 +54,8 @@ def menu_options
     ["3", "select user"],
     ["4", "select channel"],
     ["5", "details"],
-    ["6","quit"]
+    #["6", "send message"],
+    ["6","quit"] # change to option 7
   ]
   menu_table = Terminal::Table.new :headings => ["#", "Action"], :rows => numbered_options
   puts menu_table
