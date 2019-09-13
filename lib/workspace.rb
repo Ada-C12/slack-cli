48a258ef25d21fdba14921f36101a6300cf21d66
Georgina Sanchez Buenrostro <cpgeorgina.sanchez@gmail.com>
2019-09-12 18:03:46 -0700

    assigns selected object to selected instance variable

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -3,6 +3,8 @@ require "pry"
 Dotenv.load
 
 class Workspace
+  attr_reader :users, :channels
+  attr_accessor :selected 
   def initialize(users, channels, selected: nil)
     @users = users
     @channels = channels
@@ -15,12 +17,29 @@ class Workspace
     Channel.list.each do |channel|
       if channel.name == selection || channel.slack_id == selection
         value = 1
+        @selected = channel
         return channel
       end
     end
 
     if value = 0
-      raise ArgumentError.new("That is wrong") 
+      raise ArgumentError.new("Please provide a valid selection for channel. ") 
+    end
+  end 
+
+  def select_user(selection)
+    value = 0
+
+    User.list.each do |user|
+      if user.name == selection || user.slack_id == selection
+        value = 1
+        @selected = user 
+        return user
+      end
+    end
+
+    if value = 0
+      raise ArgumentError.new("Please provide a valid selection for user.") 
     end
   end 
 end
--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -15,9 +15,20 @@ describe "Workspace" do
   end
 
   describe "Workspace instantiation" do
-      it "is an instance of Workspace" do
-        expect(@workspace).must_be_kind_of Workspace
+    it "is an instance of Workspace" do
+      expect(@workspace).must_be_kind_of Workspace
     end
+
+    it "gets the selected object from the select_user method" do
+      VCR.use_cassette("workspace") do
+        users = User.list
+        channels = Channel.list
+        workspace_1 = Workspace.new(users, channels)
+        workspace_1.select_user("UMUS3K402")
+        expect(workspace_1.selected).must_be_instance_of User
+      end
+    end 
+
   end
 
   describe "select_channel" do
@@ -36,5 +47,42 @@ describe "Workspace" do
         expect {(@workspace.select_channel("Pizza"))}.must_raise ArgumentError
       end
     end
+  end  
+  
+  describe "select_user" do
+    it "returns a selected user instance" do
+      VCR.use_cassette("workspace") do
+        #Act
+        selected_user = @workspace.select_user("UMW5P9DHR")
+
+        #Assert
+        expect(selected_user).must_be_instance_of User
+      end
+    end
+
+    it "raises an argument error for invalid selection" do
+      VCR.use_cassette("workspace") do
+        expect {(@workspace.select_user("Pizza"))}.must_raise ArgumentError
+      end
+    end
+
+    it "assigns selected object to selected instance variable" do
+      VCR.use_cassette("workspace") do
+        # selection = "random"
+        hello ="world"
+        hi ="world"
+        expect(hello).must_equal hi
+        @workspace.select_channel("random")
+
+        topic = {
+        "value": "Non-work banter and water cooler conversation",
+        "creator": "UMTGXDXD0",
+        "last_set": 1568073948
+        }
+        result = Channel.new("CN5S0B30U", "random", topic, 4)
+
+        expect(@workspace.selected).must_be_instance_of Channel
+      end
+    end 
   end    
 end
