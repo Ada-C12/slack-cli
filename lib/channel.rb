3ce25109cc60db0f1f06393a710a7d19eb63b475
emaust <erikamaust@gmail.com>
2019-09-14 15:03:43 -0700

    Purpose added to self.list

--- lib/channel.rb
+++ lib/channel.rb
@@ -2,33 +2,37 @@ require_relative 'recipient'
 
 
 class Channel < Recipient
-  attr_reader :topic, :member_count
+  attr_reader :topic, :member_count, :purpose
   
-  def initialize(slack_id:, name:, topic:, member_count:)
+  def initialize(slack_id:, name:, topic:, member_count:, purpose:)
     super(slack_id: slack_id, name: name)
     @topic = topic
     @member_count = member_count
+    @purpose = purpose
   end
   
   
   def details
+    if Workspace.selected != nil
+      Workspace.selected
+      
+    end
     
-    
-  end
-  
-  # Source Citation Paige and Angele
-  def self.list
-    channels = self.get("https://slack.com/api/conversations.list")["channels"]
-
-    channels.map do |channel|
-      name = channel["name"]
-      topic = channel["topic"]["value"]
-      member_count = channel["num_members"]
-      slack_id = channel["id"]
-
-      Channel.new(slack_id: slack_id, name: name, topic: topic, member_count: member_count)
+    # Source Citation Paige and Angele
+    def self.list
+      channels = self.get("https://slack.com/api/conversations.list")["channels"]
+      
+      channels.map do |channel|
+        name = channel["name"]
+        topic = channel["topic"]["value"]
+        member_count = channel["num_members"]
+        slack_id = channel["id"]
+        purpose = channel["purpose"]
+        
+        Channel.new(slack_id: slack_id, name: name, topic: topic, member_count: member_count, purpose: purpose)
+      end
     end
   end
-end
-
-
+  
+  
+  
