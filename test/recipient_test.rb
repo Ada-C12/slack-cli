b909f0ee8c49aa06318e1aa22c5ccf87d8ae5f9c
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-09-14 13:53:34 -0700

    Added tests for Recipient.details and Recipient.self.list

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -8,40 +8,58 @@ describe "Recipient" do
         expect(recipient).must_be_kind_of Slack::Recipient
       end
     end    
+    
     it "checks data structures at initialize" do
       VCR.use_cassette("recipient-info") do      
         recipient = Slack::Recipient.new(1, "spongebob")
         expect(recipient).must_be_kind_of Slack::Recipient
       end
     end
+  end
+  
+  describe "details" do
+    it "raises an error if implementation attempted within Recipient class" do
+      VCR.use_cassette("recipient-info") do      
+        expect{ Slack::Recipient.details
+        }.must_raise Exception
+      end
+    end
+  end
+
+  describe "self.list" do
+    it "raises an error if implementation attempted within Recipient class" do
+      VCR.use_cassette("recipient-info") do      
+        expect{ Slack::Recipient.self.list
+        }.must_raise Exception
+      end
+    end
+  end
+  
+  describe "self.get" do 
+    it "user returns instance of HTTParty" do
+      VCR.use_cassette("recipient-info") do
+        user = Slack::Recipient.get("https://slack.com/api/users.list")
+        expect(user).must_be_kind_of HTTParty::Response
+      end
+    end 
     
-    
-    describe "self.get" do 
-      it "user returns instance of HTTParty" do
-        VCR.use_cassette("recipient-info") do
-          user = Slack::Recipient.get("https://slack.com/api/users.list")
-          expect(user).must_be_kind_of HTTParty::Response
-        end
-      end 
-      
-      it "channel returns an instance of HTTParty" do
-        VCR.use_cassette("recipient-info") do
-          channel = Slack::Recipient.get("https://slack.com/api/conversations.list")
-          expect(channel).must_be_kind_of HTTParty::Response
-        end
-      end 
-      
-      describe "send message" do
-        it "raises an error if invalid ID is provided" do
-          VCR.use_cassette("recipient-info") do
-            
-            slack_id = "INVALID"
-            message = "I exist only in the ether"
-            expect{ response = Slack::Recipient.send_message(slack_id, message)
-            }.must_raise Exception
-          end
-        end
-      end 
+    it "channel returns an instance of HTTParty" do
+      VCR.use_cassette("recipient-info") do
+        channel = Slack::Recipient.get("https://slack.com/api/conversations.list")
+        expect(channel).must_be_kind_of HTTParty::Response
+      end
     end 
   end
+  
+  describe "send message" do
+    it "raises an error if invalid ID is provided" do
+      VCR.use_cassette("recipient-info") do
+        
+        slack_id = "INVALID"
+        message = "I exist only in the ether"
+        expect{ Slack::Recipient.send_message(slack_id, message)
+        }.must_raise Exception
+      end
+    end 
+  end 
 end
