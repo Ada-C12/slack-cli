f77a317170176fce5a5ea78cc69b9a6cd2447c38
gracexinran <gracexinran@gmail.com>
2019-09-13 17:12:10 -0700

    Created a unit test for 'Slack_Api_Error' class

--- test/slack_api_error_test.rb
+++ test/slack_api_error_test.rb
@@ -1,6 +1,7 @@
 require_relative 'test_helper'
 
 describe 'class slack_api_error' do 
-
-
+  it "creates a new instance of Slack::Slack_Api_Error" do
+    expect(Slack::Slack_Api_Error.new).must_be_instance_of Slack::Slack_Api_Error
+  end
 end
