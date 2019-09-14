c4686c5366b900439f3e040c522ed65e06f0d343
emaust <erikamaust@gmail.com>
2019-09-13 17:36:48 -0700

    Workspace_test for instantiate passing

--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -1,2 +1,10 @@
 require_relative 'test_helper'
 require 'pry'
+
+describe "Workspace" do
+  it "instantiates Workspace" do
+    VCR.use_cassette("workspace_test") do
+      expect(Workspace.new).must_be_instance_of Workspace
+    end
+  end
+end
